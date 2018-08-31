0d7ba91ed9e6ca0d0f8ef7dd2b3c35c3a06c64ce
Chinwendu (Chin) Okoronkwo <ChinO@centershealthcare.org>
2018-08-31 14:11:00 -0400

    INVOICES - implemented modular structure to base html page and how resource pages are built / completed main navigation menu

--- app/assets.py
+++ app/assets.py
@@ -2,22 +2,22 @@ from flask_assets import Bundle
 
 def create_assets(assets):
     js = Bundle(
-         'js/grayscale.min.js',
-         'vendor/jquery/jquery.min.js',
+         #'vendor/jquery/jquery.min.js',
          'vendor/jquery-easing/jquery.easing.min.js',
-         'vendor/bootstrap/js/bootstrap.bundle.min.js',
+         #'vendor/bootstrap/js/bootstrap.bundle.min.js',
+         'js/grayscale.min.js',
          output='js/libs.js'
     )
     assets.register('JS_FRAMEWORS', js)
 
     css = Bundle(
-        'vendor/bootstrap/css/bootstrap.css',
-        'vendor/fontawesome-free/css/all.css',
+        #'vendor/bootstrap/css/bootstrap.min.css',
+        'vendor/fontawesome-free/css/all.min.css',
+        'css/grayscale.css',
+        'css/sticky-footer.css',
         #'vendor/bootstrap/css/bootstrap-grid.css',
         #'vendor/bootstrap/css/bootstrap-reboot.css',
         #'vendor/bootstrap/css/bootstrap-grid.css',
-        'css/grayscale.css',
-        #'css/sticky-footer.css',
         output='css/min.css'
     )
     assets.register('CSS_FRAMEWORKS', css)
